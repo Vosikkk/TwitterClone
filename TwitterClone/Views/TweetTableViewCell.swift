fdc5ea9834c4abb3680c027e05d4fd29fde01cfe
Vosik <vosikvos@gmail.com>
2023-10-06 16:21:25 +0300

    added custom cell and tableView

---
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -0,0 +1,96 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterClone
+//
+//  Created by Саша Восколович on 06.10.2023.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+   static let identifier = "TweetTableViewCell"
+    
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 25
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(systemName: "person.fill")
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let displayNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sasha Voskolovych"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@Vosik"
+        label.textColor = .secondaryLabel
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let tweetTextContentLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This my Mockup tweet. It is going to take multiple lines. I believe some more text is enough but let some more anyway .. and cheers youtube "
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(avatarImageView)
+        contentView.addSubview(displayNameLabel)
+        contentView.addSubview(userNameLabel)
+        contentView.addSubview(tweetTextContentLabel)
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configureConstraints() {
+        let avatarImageViewConstraints = [
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 14),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 50),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let displayNameLabelConstraints = [
+            displayNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 20),
+            displayNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+        ]
+        
+        let userNameLabelConsytraints = [
+            userNameLabel.leadingAnchor.constraint(equalTo: displayNameLabel.trailingAnchor, constant: 10),
+            userNameLabel.centerYAnchor.constraint(equalTo: displayNameLabel.centerYAnchor)
+        ]
+        
+        let tweetTextContentLabelConstraints = [
+            tweetTextContentLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            tweetTextContentLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: 10),
+            tweetTextContentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            tweetTextContentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ]
+        NSLayoutConstraint.activate(displayNameLabelConstraints)
+        NSLayoutConstraint.activate(avatarImageViewConstraints)
+        NSLayoutConstraint.activate(userNameLabelConsytraints)
+        NSLayoutConstraint.activate(tweetTextContentLabelConstraints)
+    }
+}
