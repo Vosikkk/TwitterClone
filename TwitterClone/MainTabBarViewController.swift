4de101547b977089de4172b0ab9f2711946efb51
Vosik <vosikvos@gmail.com>
2023-10-06 14:23:10 +0300

    Initial Commit

---
+++ TwitterClone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  TwitterClone
+//
+//  Created by Саша Восколович on 06.10.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
