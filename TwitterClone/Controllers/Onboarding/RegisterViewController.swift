732982cf7b055d41b0f3667e3cc50b4b027a2217
Vosik <vosikvos@gmail.com>
2023-10-16 16:23:33 +0300

    implemented onboarding

---
+++ TwitterClone/Controllers/Onboarding/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  TwitterClone
+//
+//  Created by Саша Восколович on 16.10.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
